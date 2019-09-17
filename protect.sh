cf1662a9ba3e18450f3c53b0268f737f58980a28
Jongwoo-Han <han980817@gmail.com>
2019-09-17 17:43:11 +0900

    Added Protectgrass Func

---
+++ protect.sh
@@ -0,0 +1,9 @@
+date >> /home/hanjongwoo/protectgrass/history.log
+
+cd /home/hanjongwoo/protectgrass/
+
+/usr/bin/git add . >> /home/hanjongwoo/protectgrass/history.log
+
+/usr/bin/git commit -m "Protected Grass" >> /home/hanjongwoo/protectgrass/history.log
+
+/usr/bin/git push origin master >> /home/hanjongwoo/protectgrass/history.log
