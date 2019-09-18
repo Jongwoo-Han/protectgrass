65c762c86626a8cd4acae135a8547b522a517f83
Jongwoo-Han <han980817@gmail.com>
2019-09-18 12:09:21 +0900

    Modified protect.sh

--- protect.sh
+++ protect.sh
@@ -2,6 +2,8 @@ date >> /home/hanjongwoo/protectgrass/history.log
 
 cd /home/hanjongwoo/protectgrass/
 
+/usr/bin/git pull
+
 /usr/bin/git add . >> /home/hanjongwoo/protectgrass/history.log
 
 /usr/bin/git commit -m "Protected Grass" >> /home/hanjongwoo/protectgrass/history.log
