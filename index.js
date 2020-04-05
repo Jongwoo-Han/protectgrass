d8c137c39eebb2aceff021f26cf1e8919bf39e45
jongwooo <han980817@gmail.com>
2020-04-06 02:54:58 +0900

    Update index.js to format code

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
 const moment = require("moment");
 require("moment-timezone");
 
 moment.tz.setDefault("Asia/Seoul");
-moment.locale('ko', {
-    weekdays: ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"],
-    weekdaysShort: ["일","월","화","수","목","금","토"],
+moment.locale("ko", {
+	weekdays: [
+		"일요일",
+		"월요일",
+		"화요일",
+		"수요일",
+		"목요일",
+		"금요일",
+		"토요일",
+	],
+	weekdaysShort: ["일", "월", "화", "수", "목", "금", "토"],
 });
 
 const date = `${moment().format("YYYY. MM. DD. (ddd) hh:mm:ss")} KST`;
 
-fs.writeFile('history.log', date ,'utf8', function(error){
-    if (error) throw error;
-    console.log(`Commit log: ${date}`);
+fs.writeFile("history.log", date, "utf8", function (error) {
+	if (error) throw error;
+	console.log(`Commit log: ${date}`);
 });
